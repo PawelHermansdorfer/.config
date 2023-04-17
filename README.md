99f6f0fd7a9dc80c23f360d1ed9ffa466b2d059f
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2023-04-17 02:04:00 +0200

    Hello

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+# Windows
+```console
+$ Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+$ Install-Module -Name Recycle -Force
+$ Install-Module -Name z -Force
+$ Install-Module -Name PSReadLine -Force
+$ Install-Module -Name PSFzf -Force
+$ Install-Module -Name Pscx -Force
+$ echo ". $env:USERPROFILE\.config\powershell\user_profile.ps1" >> $PROFILE.CurrentUserCurrentHost
+$ mklink $env:USERPROFILE\Appdata\local\nvim $env:USERPROFILE\.config\nvim
+$ mklink $env:USERPROFILE\Appdata\local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json $env:USERPROFILE\.config\windows_terminal\settings.json
+```
+
 # Font
 - [Liberation Mono Nerd Font]
 
--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,8 +1,28 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-# oh-my-posh
+# Prompt
+function prompt
+{
+    "$Env:username $((''+$PWD).replace($HOME, '~')) $ "
+}
+
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name nvide -Value neovide
+Set-Alias -Name ll -Value ls
+Set-Alias -Name open -Value explorer
+Set-Alias -Name grep -Value findstr
+
+
+# Remove to bin
+Set-Alias -Name rm -Value Remove-ItemSafely
+
+
+# Cool icons
 Import-Module -Name Terminal-Icons
+
 
 # PSReadLine
 Set-PSReadlineOption -EditMode Emacs
@@ -14,25 +34,127 @@ Set-PSReadLineKeyHandler -Chord 'Ctrl+k' -Function PreviousHistory
 Set-PSReadLineKeyHandler -Chord 'Ctrl+RightArrow' -Function NextWord
 Set-PSReadLineKeyHandler -Chord 'Ctrl+LeftArrow' -Function BackwardWord
 Set-PSReadLineKeyHandler -Chord 'Ctrl+Backspace' -Function BackwardDeleteWord
+Set-PSReadLineKeyHandler -Chord 'Ctrl+Delete' -Function DeleteWord
 
-# Fzf
-# Import-Module PSFzf
+
+# Fuzzy finder
+Import-Module -Name PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# Alias
-Set-Alias -Name vim -Value nvim
-Set-Alias -Name nvide -Value neovide
-Set-Alias -Name ll -Value ls
-Set-Alias -Name open -Value explorer
-Set-Alias -Name touch -Value ni
-Set-Alias -Name rm -Value Remove-ItemSafely
 
 # Initialize mscv build tools
-# Import-VisualStudioVars 2022 amd64
+$found_vcvarsall = $True
+if(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2011
+}
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2012
+}
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2013
+}
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2014
+}
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2015
+}
+# 2017+
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2017
+}
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2017
+}
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2019
+}
+elseif(Test-Path -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2019
+}
+# 2022 +
 
+elseif(Test-Path -Path "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2022
+}
+elseif(Test-Path -Path "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" -PathType Leaf)
+{
+    $vs_year = 2022
+}
+# Not found
+else
+{
+    Write-Output "Cannot find vcvarsall.bat.\nYou must have installed Visual Studio"
+    $found_vcvarsall = $False
+}
+
+$pointerSizeInBytes = (4, 8)[[Environment]::Is64BitOperatingSystem]
+if($found_vcvarsall -eq $True)
+{
+    if($pointerSizeInBytes -eq 8)
+    {
+        Import-VisualStudioVars $vs_year amd64
+    }
+    else
+    {
+        Import-VisualStudioVars $vs_year amd64_x86
+    }
+}
+
+
+
+
+
+# Touch
+Remove-Item 'Alias:\touch' -Force
+Function touch {
+    [cmdletbinding()]
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
 
 # Which
 function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    if($command -eq $null) {
+        throw "No command supplied"
+    }
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Mklink
+function mklink ($link, $target) {
+    if($link -eq $null) {
+        throw "Link file not supplied"
+    }
+    if($target -eq $null) {
+        throw "Target file not supplied"
+    }
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
+# CheckFile
+function CheckFile($file_path)
+{
+     Test-Path -Path $file_path -PathType Leaf
 }
